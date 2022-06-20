3e8406017916cf8b3bc71f98d05c6b267ce7dda9
KainiChang <changkaini91@gmail.com>
2022-06-20 12:02:07 +0930

    first submit Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
+
+const [value, setState] = React.useState(0)
+
+function minus (){
+  return setState(prev => prev-1 )
+}
+function add (){
+  return setState(prev => prev+1 )
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App--operator" onClick={minus}>
+-
+      </div>
+      <div className="App--value">
+{value}
+      </div>
+      <div className="App--operator" onClick={add}>
++
+</div>
     </div>
   );
 }
